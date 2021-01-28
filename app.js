8f76dc6cb000577c2ce25baba84ea537b03304a8
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-01-29 09:43:24 +1100

    Error Handler created

--- app.js
+++ app.js
@@ -10,5 +10,22 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/products', ProductRoute);
 
+//error middleware
+app.use((req, res, next) => {
+  const err = new Error('Page Not Found.');
+  err.status = 404;
+  next(err);
+});
+//error handler
+app.use((err, re, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`PORT running at port ${PORT}.`));
