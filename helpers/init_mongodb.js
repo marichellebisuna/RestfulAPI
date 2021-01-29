2bae9e99c5f040b8bbbff5e900ae087eeae3c95e
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-01-29 12:19:21 +1100

    Product model created

--- helpers/init_mongodb.js
+++ helpers/init_mongodb.js
@@ -12,3 +12,18 @@ mongoose
     console.log('Mongodb connected.');
   })
   .catch((err) => console.log(err.message));
+
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connected.');
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected.');
+});
+mongoose.connection.on('error', (err) => {
+  console.log(err.message);
+});
+
+process.on('SIGINT', async () => {
+  await mongoose.connection.close();
+  process.exit(0);
+});
