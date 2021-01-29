0c0bb1039d33c2dfc5c902578110187d75e28811
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-01-29 13:09:11 +1100

    getting all products created

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -2,8 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../Models/Product.model');
 
-router.get('/', (req, res, next) => {
-  res.send('list of all products.');
+router.get('/', async (req, res, next) => {
+  try {
+    // to list all the product with just name and price only
+    const result = await Product.find({}, { name: 1, price: 1, _id: 0 });
+
+    // to list all products of specific price only
+    // const result = await Product.find({ price: 499 }, {});
+
+    res.send(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 router.post('/', async (req, res, next) => {
