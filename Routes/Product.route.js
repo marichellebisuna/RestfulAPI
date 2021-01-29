7af70a8c7ae636fe396532387b328762261586cf
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-01-29 12:56:44 +1100

    Saving product created.

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../Models/Product.model');
 
 router.get('/', (req, res, next) => {
   res.send('list of all products.');
 });
 
-router.post('/', (req, res, next) => {
-  res.send('create a product');
+router.post('/', async (req, res, next) => {
+  try {
+    const product = new Product(req.body);
+    const result = await product.save();
+    res.send(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  // const product = new Product({
+  //   name: req.body.name,
+  //   price: req.body.price,
+  // });
+  // product
+  //   .save()
+  //   .then((result) => {
+  //     console.log(result);
+  //     res.send(result);
+  //   })
+  //   .catch((err) => console.log(err.message));
 });
 
 router.get('/:id', (req, res, next) => {
